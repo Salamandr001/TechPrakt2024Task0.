e72691c9bdf8205c4ddab9c346584dc7f3d60b5c
Міняйло Юрій <faerskorpion@gmail.com>
2024-02-03 16:41:27 +0200

    task1

---
+++ task1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введіть перше число:");
+         
+            string input1 = Console.ReadLine();
+            
+            double number1 = Convert.ToDouble(input1);
+
+            Console.WriteLine("Введіть друге число:");
+            
+            string input2 = Console.ReadLine();
+           
+            double number2 = Convert.ToDouble(input2);
+
+           
+            double sum = number1 + number2;
+
+            
+            Console.WriteLine("Сума чисел: " + sum);
+
+            Console.ReadLine();
+        }
+    }
+}
